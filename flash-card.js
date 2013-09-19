c4ce17f0c7f9d41fcbf78c88f73638807fd2f889
Jayson Falkner <jayson@counsyl.com>
2013-09-18 22:01:41 -0700

    Added list of questions.

--- flash-card.js
+++ flash-card.js
@@ -1,3 +1,29 @@
+var questions = [
+    {question: "I am a question",
+     answer: "I am an answer"},
+    {question: "Second question.",
+     answer: "Second answer."},
+    {question: "Third question.",
+     answer: "Third answer."},
+]
+var index = 0;
+
 $(document).ready(function(){
-    window.alert("I Loaded");
+
+    // Load by default the first question
+    $('#card').html(questions[index]["question"])
+
+    // When clicked, switch to show the answer.
+    $('#card').click(function(){
+        card = $('#card')
+        text = card.html()
+        if (text == questions[index]["question"]) {
+            card.html(questions[index]["answer"])
+            index++;
+        }
+        else {
+            card.html(questions[index]["question"])
+        }
+    })
 });
+
